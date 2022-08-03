231b115b5c240a1fd535037d58327f74ed04b47f
Lortiz528 <lortiztech@gmail.com>
2022-08-03 09:57:00 -0400

    create and :id routes set up

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -10,4 +10,30 @@ songs.get('/', async (req, res) => {
   res.json({ success: true, payload: allSongs });
 });
 
+//Individual
+songs.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const indySong = await db.one('SELECT * FROM song WHERE id=$1', id);
+  if (indySong) {
+    res.status(200).json({ success: true, payload: indySong });
+  } else {
+    res.status(404).send(`No such song with id of ${id}`);
+  }
+});
+
+//create
+songs.post('/new', async (req, res) => {
+  const newSong = await db.any(
+    'INSERT INTO song (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+    [
+      req.body.name,
+      req.body.artist,
+      req.body.album,
+      req.body.time,
+      req.body.is_favorite,
+    ]
+  );
+  res.status(200).json({ success: true, payload: newSong });
+});
+
 module.exports = songs;
